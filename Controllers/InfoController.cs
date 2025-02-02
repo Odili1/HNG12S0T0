c728defad38872ffa5b2af0a0b76e3202eea7bc8
Odili1 <ezedigwegerald1@gmail.com>
2025-02-02 16:21:31 +0100

    'format_date'

--- Controllers/InfoController.cs
+++ Controllers/InfoController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace HNG12S0T0.Controllers
 {
     [ApiController]
-    [Route("/info")]
+    [Route("/")]
     public class InfoController : ControllerBase
     {
         [HttpGet]
@@ -12,7 +12,7 @@ namespace HNG12S0T0.Controllers
             var response = new
             {
                 email = "ezedigwegerald1@gmail.com",
-                current_datetime = DateTime.UtcNow.ToString("o"),
+                current_datetime = DateTime.UtcNow.ToString("yyy-MM-ddTHH:mm:ssZ"),
                 github_url = "https://github.com/Odili1/HNG12S0T0"
             };
 
